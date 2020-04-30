482741d61af7aa25a1dfe6c5d03ea8c9a26dd759
Vladimir Grichina <vgrichina@gmail.com>
2020-04-29 18:27:04 -0700

    More UI refactoring using macros

--- workdir/near-ledger-app/src/ui/nanos/ui_menus_nanos.h
+++ workdir/near-ledger-app/src/ui/nanos/ui_menus_nanos.h
@@ -4,50 +4,38 @@
 #include "os.h"
 #include "cx.h"
 
-const bagl_element_t ui_address_nanos[] = {
-    // type                               userid    x    y   w    h  str rad
-    // fill      fg        bg      fid iid  txt ]
-    {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF,
-      0, 0},
-     NULL},
+//    type          userid  x  y  w    h str rad fill       fg        bg      fid iid  txt 
+#define EMPTY_BG() \
+    {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF, 0, 0}, NULL}
 
-    {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
-      BAGL_GLYPH_ICON_CROSS},
-     NULL},
-    {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
-      BAGL_GLYPH_ICON_CHECK},
-     NULL},
+//    type     userid  x  y   w  h str rad fill fg     bg      fid  iid    txt
+#define LEFT_ICON(icon) \
+    {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0, icon}, NULL}
+#define RIGHT_ICON(icon) \
+    {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0, icon}, NULL}
 
-    //{{BAGL_ICON                           , 0x01,  31,   9,  14,  14, 0, 0, 0
-    //, 0xFFFFFF, 0x000000, 0, BAGL_GLYPH_ICON_EYE_BADGE  }, NULL, 0, 0, 0,
-    //NULL, NULL, NULL },
-    {{BAGL_LABELINE, 0x01, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
-      BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "Confirm"},
-    {{BAGL_LABELINE, 0x01, 0, 26, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
-      BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "address"},
-};
-
+//    type       userid  x  y   w    h  str rad fill    fg        bg
+//    fid                                                            iid  txt
 #define INFO_CELL(id, title, info)  \
     {{BAGL_LABELINE, id, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000, \
       BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0}, title}, \
     {{BAGL_LABELINE, id, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000, \
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0}, info}
 
-const bagl_element_t ui_verify_transfer_nanos[] = {
-    // type                               userid    x    y   w    h  str rad
-    // fill      fg        bg      fid iid  txt ]
-    {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF,
-      0, 0},
-     NULL},
+const bagl_element_t ui_address_nanos[] = {
+    EMPTY_BG(),
 
-    {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
-      BAGL_GLYPH_ICON_CROSS},
-     NULL},
-    {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
-      BAGL_GLYPH_ICON_CHECK},
-     NULL},
+    LEFT_ICON(BAGL_GLYPH_ICON_CROSS),
+    RIGHT_ICON(BAGL_GLYPH_ICON_CHECK),
+
+    INFO_CELL(0x01, "Confirm", "public key")
+};
+
+const bagl_element_t ui_verify_transfer_nanos[] = {
+    EMPTY_BG(),
+
+    LEFT_ICON(BAGL_GLYPH_ICON_CROSS),
+    RIGHT_ICON(BAGL_GLYPH_ICON_CHECK),
 
     // TODO: Why ui_context is using unsigned char*? Should we just use char* everywhere?
     INFO_CELL(0x01, "Confirm", "transfer"),
@@ -56,22 +44,13 @@ const bagl_element_t ui_verify_transfer_nanos[] = {
     INFO_CELL(0x04, "From", (char *) ui_context.line3)
 };
 
-
 // TODO: Use macros to refactor these (as really just labels and ids are different)
 
 const bagl_element_t ui_verify_transaction_nanos[] = {
-    // type                               userid    x    y   w    h  str rad
-    // fill      fg        bg      fid iid  txt  ]
-    {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF,
-      0, 0},
-     NULL},
+    EMPTY_BG(),
 
-    {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
-      BAGL_GLYPH_ICON_CROSS},
-     NULL},
-    {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
-      BAGL_GLYPH_ICON_CHECK},
-     NULL},
+    LEFT_ICON(BAGL_GLYPH_ICON_CROSS),
+    RIGHT_ICON(BAGL_GLYPH_ICON_CHECK),
 
     INFO_CELL(0x01, "Confirm", (char *) ui_context.line1),
     INFO_CELL(0x02, "To", (char *) ui_context.line2),
