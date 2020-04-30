3508b7331178034bd7dbe44a5fd0a06536022e7d
Vladimir Grichina <vgrichina@gmail.com>
2020-04-29 17:01:10 -0700

    Fix most compile warnings in ui_menus_nanos

--- workdir/near-ledger-app/src/ui/nanos/ui_menus_nanos.h
+++ workdir/near-ledger-app/src/ui/nanos/ui_menus_nanos.h
@@ -6,105 +6,108 @@
 
 const bagl_element_t ui_address_nanos[] = {
     // type                               userid    x    y   w    h  str rad
-    // fill      fg        bg      fid iid  txt   touchparams...       ]
+    // fill      fg        bg      fid iid  txt ]
     {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF,
       0, 0},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
 
     {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
       BAGL_GLYPH_ICON_CROSS},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
     {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
       BAGL_GLYPH_ICON_CHECK},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
 
     //{{BAGL_ICON                           , 0x01,  31,   9,  14,  14, 0, 0, 0
     //, 0xFFFFFF, 0x000000, 0, BAGL_GLYPH_ICON_EYE_BADGE  }, NULL, 0, 0, 0,
     //NULL, NULL, NULL },
     {{BAGL_LABELINE, 0x01, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "Confirm", 0, 0, 0, NULL, NULL, NULL},
+     "Confirm"},
     {{BAGL_LABELINE, 0x01, 0, 26, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "address", 0, 0, 0, NULL, NULL, NULL},
+     "address"},
 };
 
 const bagl_element_t ui_verify_transfer_nanos[] = {
     // type                               userid    x    y   w    h  str rad
-    // fill      fg        bg      fid iid  txt   touchparams...       ]
+    // fill      fg        bg      fid iid  txt ]
     {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF,
       0, 0},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
 
     {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
       BAGL_GLYPH_ICON_CROSS},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
     {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
       BAGL_GLYPH_ICON_CHECK},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
 
     {{BAGL_LABELINE, 0x01, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "Confirm", 0, 0, 0, NULL, NULL, NULL},
+     "Confirm"},
     {{BAGL_LABELINE, 0x01, 0, 26, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "transfer", 0, 0, 0, NULL, NULL, NULL},
+     "transfer"},
 
     {{BAGL_LABELINE, 0x02, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "Amount", 0, 0, 0, NULL, NULL, NULL},
+     "Amount"},
     {{BAGL_LABELINE, 0x02, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
-     (const char *)ui_context.line1, 0, 0, 0, NULL, NULL, NULL},
+     (const char *)ui_context.line1},
 
     {{BAGL_LABELINE, 0x03, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-      "To", 0, 0, 0, NULL, NULL, NULL},
+      "To"},
      {{BAGL_LABELINE, 0x03, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000,
        BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
-      (const char *)ui_context.line2, 0, 0, 0, NULL, NULL, NULL},
+      (const char *)ui_context.line2},
 
     {{BAGL_LABELINE, 0x04, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "From", 0, 0, 0, NULL, NULL, NULL},
+     "From"},
     {{BAGL_LABELINE, 0x04, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
-     (const char *)ui_context.line3, 0, 0, 0, NULL, NULL, NULL},
+     (const char *)ui_context.line3},
 };
+
+
+// TODO: Use macros to refactor these (as really just labels and ids are different)
 
 const bagl_element_t ui_verify_transaction_nanos[] = {
     // type                               userid    x    y   w    h  str rad
-    // fill      fg        bg      fid iid  txt   touchparams...       ]
+    // fill      fg        bg      fid iid  txt  ]
     {{BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000, 0xFFFFFF,
       0, 0},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
 
     {{BAGL_ICON, 0x00, 3, 12, 7, 7, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
       BAGL_GLYPH_ICON_CROSS},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
     {{BAGL_ICON, 0x00, 117, 13, 8, 6, 0, 0, 0, 0xFFFFFF, 0x000000, 0,
       BAGL_GLYPH_ICON_CHECK},
-     NULL, 0, 0, 0, NULL, NULL, NULL},
+     NULL},
     {{BAGL_LABELINE, 0x01, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "Confirm", 0, 0, 0, NULL, NULL, NULL},
+     "Confirm"},
     {{BAGL_LABELINE, 0x01, 0, 26, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     (const char *) ui_context.line1, 0, 0, 0, NULL, NULL, NULL},
+     (const char *) ui_context.line1},
 
     {{BAGL_LABELINE, 0x02, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-      "To", 0, 0, 0, NULL, NULL, NULL},
+      "To"},
      {{BAGL_LABELINE, 0x02, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000,
        BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
-      (const char *)ui_context.line2, 0, 0, 0, NULL, NULL, NULL},
+      (const char *)ui_context.line2},
 
     {{BAGL_LABELINE, 0x03, 0, 12, 128, 12, 0, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-     "From", 0, 0, 0, NULL, NULL, NULL},
+     "From"},
     {{BAGL_LABELINE, 0x03, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
-     (const char *)ui_context.line3, 0, 0, 0, NULL, NULL, NULL},
+     (const char *)ui_context.line3},
 };
 
 const ux_menu_entry_t menu_about[];
