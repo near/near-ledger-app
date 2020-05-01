874f5ede6341bc9521dda95b9dd8c9243e76cb78
Vladimir Grichina <vgrichina@gmail.com>
2020-04-30 19:19:19 -0700

    Consolidate _prepro functions to use same logic

--- workdir/near-ledger-app/src/ui/nanos/ui_menus_prepro.c
+++ workdir/near-ledger-app/src/ui/nanos/ui_menus_prepro.c
@@ -2,51 +2,8 @@
 
 #include "../ui.h"
 
-// TODO: These look repetitive and likely not matching what we want in terms of UX. Figure out how to refactor
-
-const bagl_element_t * ui_address_prepro(const bagl_element_t *element) {
-    if (element->component.userid > 0) {
-        unsigned int display = (ux_step == element->component.userid - 1);
-        if (display) {
-            switch (element->component.userid) {
-            case 1:
-                UX_CALLBACK_SET_INTERVAL(2000);
-                break;
-            case 2:
-                UX_CALLBACK_SET_INTERVAL(MAX(
-                    3000, 1000 + bagl_label_roundtrip_duration_ms(element, 7)));
-                break;
-            }
-        }
-        if (!display)
-             return NULL;
-    }
-    return element;
-}
-
-// display or not according to step, and adjust delay
-const bagl_element_t * ui_verify_transfer_prepro(const bagl_element_t *element) {
-    if (element->component.userid > 0) {
-        unsigned int display = (ux_step == element->component.userid - 1);
-        if (display) {
-            switch (element->component.userid) {
-            case 1:
-                UX_CALLBACK_SET_INTERVAL(2000);
-                break;
-            case 2:
-            case 3:
-                UX_CALLBACK_SET_INTERVAL(MAX(
-                    3000, 1000 + bagl_label_roundtrip_duration_ms(element, 7)));
-                break;
-            }
-        }
-        if (!display)
-             return NULL;
-    }
-    return element;
-}
-
-const bagl_element_t * ui_verify_function_call_prepro(const bagl_element_t *element) {
+const bagl_element_t * simple_scroll_prepro(const bagl_element_t *element) {
+    // display or not according to step, and adjust delay
     if (element->component.userid > 0) {
         unsigned int display = (ux_step == element->component.userid - 1);
         if (display) {
@@ -60,23 +17,18 @@ const bagl_element_t * ui_verify_function_call_prepro(const bagl_element_t *elem
     return element;
 }
 
+const bagl_element_t * ui_address_prepro(const bagl_element_t *element) {
+    return simple_scroll_prepro(element);
+}
+
+const bagl_element_t * ui_verify_transfer_prepro(const bagl_element_t *element) {
+    return simple_scroll_prepro(element);
+}
+
+const bagl_element_t * ui_verify_function_call_prepro(const bagl_element_t *element) {
+    return simple_scroll_prepro(element);
+}
+
 const bagl_element_t * ui_verify_transaction_prepro(const bagl_element_t *element) {
-    if (element->component.userid > 0) {
-        unsigned int display = (ux_step == element->component.userid - 1);
-        if (display) {
-            switch (element->component.userid) {
-            case 1:
-                UX_CALLBACK_SET_INTERVAL(2000);
-                break;
-            case 2:
-            case 3:
-                UX_CALLBACK_SET_INTERVAL(MAX(
-                    3000, 1000 + bagl_label_roundtrip_duration_ms(element, 7)));
-                break;
-            }
-        }
-        if (!display)
-             return NULL;
-    }
-    return element;
+    return simple_scroll_prepro(element);
 }
