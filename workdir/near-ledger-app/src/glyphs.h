0dcad6c13d58f4f49ae77ff6c458c1b89ace77cb
Vladimir Grichina <vgrichina@gmail.com>
2020-05-14 18:29:32 -0700

    Remove accidental use of waves icon instead of near

--- workdir/near-ledger-app/src/glyphs.h
+++ workdir/near-ledger-app/src/glyphs.h
@@ -1,60 +1,44 @@
 #ifndef GLYPH_badge_transaction_BPP
-#define GLYPH_badge_transaction_WIDTH 50
-#define GLYPH_badge_transaction_HEIGHT 50
-#define GLYPH_badge_transaction_BPP 2
-extern
-unsigned int const C_badge_transaction_colors[]
-;
-extern	
-unsigned char const C_badge_transaction_bitmap[];
+        #define GLYPH_badge_transaction_WIDTH 50
+        #define GLYPH_badge_transaction_HEIGHT 50
+        #define GLYPH_badge_transaction_BPP 2
+extern unsigned int const C_badge_transaction_colors[];
+extern unsigned char const C_badge_transaction_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
-#include "os_io_seproxyhal.h"
-extern
-const bagl_icon_details_t C_badge_transaction;
-#endif // GLYPH_badge_transaction_BPP
-#endif // OS_IO_SEPROXYHAL
+        #include "os_io_seproxyhal.h"
+        extern const bagl_icon_details_t C_badge_transaction;
+        #endif // GLYPH_badge_transaction_BPP
+        #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_back_BPP
-#define GLYPH_icon_back_WIDTH 14
-#define GLYPH_icon_back_HEIGHT 14
-#define GLYPH_icon_back_BPP 1
-extern
-unsigned int const C_icon_back_colors[]
-;
-extern	
-unsigned char const C_icon_back_bitmap[];
+        #define GLYPH_icon_back_WIDTH 14
+        #define GLYPH_icon_back_HEIGHT 14
+        #define GLYPH_icon_back_BPP 1
+extern unsigned int const C_icon_back_colors[];
+extern unsigned char const C_icon_back_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
-#include "os_io_seproxyhal.h"
-extern
-const bagl_icon_details_t C_icon_back;
-#endif // GLYPH_icon_back_BPP
-#endif // OS_IO_SEPROXYHAL
+        #include "os_io_seproxyhal.h"
+        extern const bagl_icon_details_t C_icon_back;
+        #endif // GLYPH_icon_back_BPP
+        #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_dashboard_BPP
-#define GLYPH_icon_dashboard_WIDTH 14
-#define GLYPH_icon_dashboard_HEIGHT 14
-#define GLYPH_icon_dashboard_BPP 1
-extern
-unsigned int const C_icon_dashboard_colors[]
-;
-extern	
-unsigned char const C_icon_dashboard_bitmap[];
+        #define GLYPH_icon_dashboard_WIDTH 14
+        #define GLYPH_icon_dashboard_HEIGHT 14
+        #define GLYPH_icon_dashboard_BPP 1
+extern unsigned int const C_icon_dashboard_colors[];
+extern unsigned char const C_icon_dashboard_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
-#include "os_io_seproxyhal.h"
-extern
-const bagl_icon_details_t C_icon_dashboard;
-#endif // GLYPH_icon_dashboard_BPP
-#endif // OS_IO_SEPROXYHAL
-#ifndef GLYPH_icon_waves_BPP
-#define GLYPH_icon_waves_WIDTH 16
-#define GLYPH_icon_waves_HEIGHT 16
-#define GLYPH_icon_waves_BPP 1
-extern
-unsigned int const C_icon_waves_colors[]
-;
-extern	
-unsigned char const C_icon_waves_bitmap[];
+        #include "os_io_seproxyhal.h"
+        extern const bagl_icon_details_t C_icon_dashboard;
+        #endif // GLYPH_icon_dashboard_BPP
+        #endif // OS_IO_SEPROXYHAL
+#ifndef GLYPH_icon_near_BPP
+        #define GLYPH_icon_near_WIDTH 16
+        #define GLYPH_icon_near_HEIGHT 16
+        #define GLYPH_icon_near_BPP 1
+extern unsigned int const C_icon_near_colors[];
+extern unsigned char const C_icon_near_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
-#include "os_io_seproxyhal.h"
-extern
-const bagl_icon_details_t C_icon_waves;
-#endif // GLYPH_icon_waves_BPP
-#endif // OS_IO_SEPROXYHAL
+        #include "os_io_seproxyhal.h"
+        extern const bagl_icon_details_t C_icon_near;
+        #endif // GLYPH_icon_near_BPP
+        #endif // OS_IO_SEPROXYHAL
--- workdir/near-ledger-app/src/ui/nanos/ui_menus_nanos.h
+++ workdir/near-ledger-app/src/ui/nanos/ui_menus_nanos.h
@@ -92,7 +92,7 @@ const bagl_element_t ui_verify_transaction_nanos[] = {
 const ux_menu_entry_t menu_about[];
 
 const ux_menu_entry_t menu_main[] = {
-    {NULL, NULL, 0, &C_icon_waves, "Use wallet to", "view accounts", 33, 12},
+    {NULL, NULL, 0, &C_icon_near, "Use wallet to", "view accounts", 33, 12},
     {menu_about, NULL, 0, NULL, "About", NULL, 0, 0},
     {NULL, os_sched_exit, 0, &C_icon_dashboard, "Quit app", NULL, 50, 29},
     UX_MENU_END};
